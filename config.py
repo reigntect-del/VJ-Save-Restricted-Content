458e6e549a12ce74c66cbecb5231ef9d717de2a2
reigntect-del <reigntect@gmail.com>
2026-03-14 13:55:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,10 +17,19 @@ else:
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "38318290
+
+Api id 
+38318290
+
+App api hash 
+
+7d498a250d346d3749c4433eb7c96fb2
+
+All latest batch here"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "7d498a250d346d3749c4433eb7c96fb2")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "6073523936"))
@@ -32,7 +41,7 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://chaturareign:8Mzr09b7rZsCazDT@cluster0.sh7vnxi.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
